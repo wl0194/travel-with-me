46ebd6eead8345a136b846fa0f524ab8817aa4cf
Amy Hwang <728amyh@gmail.com>
2022-03-24 15:12:41 -0400

    set up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+   
     return (
+
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        
+                        TRVL
                     </Link>
+                  
+                    <ul className='nav-menu'>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links'>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links'>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links'>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-links'>
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
         
             </nav>
@@ -15,4 +42,4 @@ function Navbar() {
     )
 }
 
-export default Navbar
+export default Navbar;
