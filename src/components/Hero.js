db28376a592bce317499390b2ac2e3d46265a315
Amy Hwang <728amyh@gmail.com>
2022-03-30 15:27:57 -0400

    adding hero image

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="hero-container">
+            <img src="/images/hero-travel.jpeg"></img>
+        </div>
+    )
+}
+
+export default Hero
