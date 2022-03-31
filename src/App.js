66e66b9e858660c03c57390815481ef2ad0a9de4
Amy Hwang <728amyh@gmail.com>
2022-03-30 22:40:15 -0400

    added hero image

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact component= {Home} />
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
     </>
