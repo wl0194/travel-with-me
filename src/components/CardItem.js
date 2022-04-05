7ef8986e0bbc7df6f221453052e18258ffe699a0
Amy Hwang <728amyh@gmail.com>
2022-04-05 13:04:32 -0400

    added card components

---
+++ src/components/CardItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function CardItem() {
+    return (
+        <>
+            <li className="cards_item">
+                <Link className="cards_item_link">
+                    <figure className="cards_item_pic-wrap">
+                        <img src="/" alt="Travel Image"
+                            className="cards_item_img"/>
+                    </figure>
+                    <div className="cards_item_info">
+                        <h5 className="cards_item_text"/>
+                    </div>
+                </Link>
+            </li>
+        </>
+    )
+}
+
+export default CardItem;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,8 @@ function Hero() {
     return (
         <div className="hero-container">
             <img src="/src/images/hero-travel.jpg"></img>
+            <h1>Plan A Trip To Unforgettable Travel</h1>
+            <p>Discover world's most beautiful small towns</p>
         </div>
     )
 }
